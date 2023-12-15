db6e941b80fbd2c06735454f6ca502a6e4329031
Umair297 <workumair49@gmail.com>
2023-12-15 23:04:33 +0500

    11 project

---
+++ function.js
@@ -0,0 +1,11 @@
+
+let box = document.getElementById("box");
+
+function fun(){
+    if(box.style.color != "red"){
+        box.style.color = "red";
+    }
+    else{
+        box.style.color = "black";
+    }
+}
